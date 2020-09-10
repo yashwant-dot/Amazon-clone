b1bac9ac9fce6b05f8901d33e78c0f105ac92a23
yashwant-dot <yashwant_raghuwanshi@yahoo.com>
2020-09-10 21:24:20 +0530

    initial setup

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC6a2T2fVByiyY8wd_ZEbDJWl3egbW3DVU",
+  authDomain: "clone-a0601.firebaseapp.com",
+  databaseURL: "https://clone-a0601.firebaseio.com",
+  projectId: "clone-a0601",
+  storageBucket: "clone-a0601.appspot.com",
+  messagingSenderId: "940338894721",
+  appId: "1:940338894721:web:4f30eec808b40b4a5b3a80"
+};
