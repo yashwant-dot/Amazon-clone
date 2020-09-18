336c5d83ba6b9985a78cf91e70ae2eb18f3af27a
yashwant-dot <yashwant_raghuwanshi@yahoo.com>
2020-09-18 18:12:10 +0530

    added products on home

--- src/Product.js
+++ src/Product.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './Product.css';
 
-function Product() {
+function Product({title, image, price, rating}) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>The Lean Startup</p>
+        <p>{title}</p>
         <p className="product__price">
           <small>$</small>
-          <strong>19.99</strong>
+          <strong>{price}</strong>
         </p>
         <div className="product__rating">
-          <p>⭐</p>
+          {Array(rating).fill().map((_, i ) => (<p>⭐</p>))}
         </div>
       </div>
 
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSZIMaihMMBxSe7WtA-GyAmcJnDxY0h7LFVSQ&usqp=CAU" alt="book"/>
+      <img src={image} alt="book"/>
 
       <button>Add To Basket</button>
     </div>
