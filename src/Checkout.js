76dc4f9d702ae1fcb5086571792058771e260c2d
yashwant-dot <yashwant_raghuwanshi@yahoo.com>
2020-09-19 12:14:29 +0530

    completed basket functionality

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import Subtotal from './Subtotal';
 import './Checkout.css';
+import CheckoutProduct from './CheckoutProduct';
+import { useStateValue } from './StateProvider';
 
 function Checkout() {
+
+  const [{basket}, dispatch] = useStateValue();
+
   return (
     <div className="checkout">
 
@@ -13,7 +18,17 @@ function Checkout() {
 
         <div>
           <h2 className="checkout__title">Your Shopping Basket</h2>
-
+          
+          {basket.map(item => (
+            <CheckoutProduct
+              id = {item.id}
+              title = {item.title}
+              image = {item.image}
+              price = {item.price}
+              rating = {item.rating}
+            />
+          ))}
+          
         </div>
       </div>
 
--- src/reducer.js
+++ src/reducer.js
@@ -13,9 +13,26 @@ const reducer = (state, action) => {
         ...state,
         basket: [...state.basket, action.item],
       };
+    
+    case "REMOVE_FROM_BASKET":
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id == action.id
+      );
+      let newBasket = [...state.basket];
+      
+      if(index >=0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(`Cant remove the product (id: ${action.id}) as its not in the basket!`);
+      }
 
-      default:
-        return state;
+      return {
+        ...state,
+        basket: newBasket
+      }
+
+    default:
+      return state;
   }
 };
 
