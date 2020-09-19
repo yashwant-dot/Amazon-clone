c3dbbbe729734ab01d61754e52327f8f1197b924
yashwant-dot <yashwant_raghuwanshi@yahoo.com>
2020-09-19 10:20:51 +0530

    added context api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import Header from './Header';
 import Home from './Home';
+import Checkout from './Checkout';
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
       <Header />
-      <Home />
-    </div>
+      <Switch>
+
+        <Route path="/checkout">
+          <Checkout />
+        </Route>
+
+        <Route path="/">
+          <Home />
+        </Route>
+
+      </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -2,13 +2,16 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import {Link} from 'react-router-dom';
 
 function Header() {
   return (
     <div className="header">
 
-      <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" />
-
+      <Link to='/'>
+        <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" />
+      </Link>
+  
       <div className="header__search">
         <input className="header__searchInput" type="text" />
         <SearchIcon className="header__searchIcon" />
@@ -29,11 +32,13 @@ function Header() {
         </div>
       </div>
 
-      <div className="header__optionBasket">
-        <ShoppingBasketIcon />
-        <span className="header__optionLineTwo header__basketCount" >0</span>
-      </div>
-
+      <Link to='/checkout'>
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionLineTwo header__basketCount" >0</span>
+        </div>
+      </Link>
+      
     </div>
   )
 }
