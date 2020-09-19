ed5233730f5ba8ad8aff60e6653fc4775dbd37d0
yashwant-dot <yashwant_raghuwanshi@yahoo.com>
2020-09-19 10:56:01 +0530

    added subtotal functionality

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import './Product.css';
+import {useStateValue} from './StateProvider';
 
-function Product({title, image, price, rating}) {
+function Product({id, title, image, price, rating}) {
+
+  const [state, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    // dispatch the item into data layer
+    dispatch({
+      type: 'ADD_TO_BASKET',
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product__info">
@@ -17,7 +35,7 @@ function Product({title, image, price, rating}) {
 
       <img src={image} alt="book"/>
 
-      <button>Add To Basket</button>
+      <button onClick = {addToBasket}>Add To Basket</button>
     </div>
   )
 }
--- src/reducer.js
+++ src/reducer.js
@@ -2,8 +2,11 @@ export const initialState = {
   basket: []
 };
 
-const reducer = (state, action) => {
+export const getBasketTotal = (basket) =>
+  basket?.reduce((amount, item) => item.price + amount, 0);
 
+
+const reducer = (state, action) => {
   switch(action.type) {
     case "ADD_TO_BASKET":
       return {
